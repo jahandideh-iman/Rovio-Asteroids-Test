c2a43fe9daa0f7a6f5e4abd5dcdafa60092fbd58
jahandideh-iman <jahandideh_iman@yahoo.com>
2023-02-05 11:49:59 +0100

    Added basic behavior to AsteroidAvatar for splliting on damage.

--- Assets/Scripts/Gameplay/Asteroid/AsteroidAvatar.cs
+++ Assets/Scripts/Gameplay/Asteroid/AsteroidAvatar.cs
@@ -1,17 +1,67 @@
 
+using System;
 using UnityEngine;
 
 namespace Asteroids.Presentation
 {
     public class AsteroidAvatar : MonoBehaviour
     {
+        static readonly int StandardHealth = 3;
+
         [SerializeField] new Rigidbody2D rigidbody;
-        [SerializeField] Vector2 velocity;
+        [SerializeField] float speed;
+        [SerializeField] Vector2 initialDirection;
+
+        public int Health { get; private set; } // TODO: Make this configurable
 
         private void Awake()
         {
-            rigidbody.velocity = velocity;
+            Setup(3, initialDirection.normalized);
         }
-     
+
+        private void Setup(int health, Vector2 direction)
+        {
+            initialDirection = direction;
+
+            rigidbody.velocity = direction * speed;
+
+            Health = health;
+            transform.localScale = Vector3.one * ((float)Health / StandardHealth);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Bullet"))
+            {
+                Destroy(collision.gameObject);
+                TakeDamage();
+            }
+        }
+
+        private void TakeDamage()
+        {
+            Health -= 1;
+
+            if (Health <= 0)
+                return;
+
+            var asteroid1 = Instantiate(this, this.transform.parent, true);
+            var asteroid2 = Instantiate(this, this.transform.parent, true);
+
+            asteroid1.Setup(Health, RandomDirection());
+            asteroid2.Setup(Health, RandomDirection());
+
+            Destroy(this.gameObject);
+        }
+
+        private Vector2 RandomDirection()
+        {
+            var direction = new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f));
+
+            if (direction.x == 0 && direction.y == 0)
+                return new Vector2(1, 0);
+            else
+                return direction.normalized;
+        }
     }
 }
